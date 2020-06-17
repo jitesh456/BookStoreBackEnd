43294f90db0d3667c00c1af172a9cfb9d0ba3468
kn52 <ashish52922@gmail.com>
2020-06-17 19:23:37 +0530

    REFACTORED:Merge search and sort api

---
+++ src/main/java/com/bookstoreapp/comparison/Comparison.java
@@ -0,0 +1,23 @@
+package com.bookstoreapp.comparison;
+
+import com.bookstoreapp.model.Book;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Comparison {
+
+    public static List<Book> getSortedBooks(List<Book> book, String field){
+        if(field.equals("authername")){
+            book=book.stream().sorted(Comparator.comparing(x->x.authorName)).collect(Collectors.toList());
+        }
+        if(field.equals("category")){
+            book=book.stream().sorted(Comparator.comparing(x->x.category)).collect(Collectors.toList());
+        }
+        if(field.equals("price")) {
+            book=book.stream().sorted(Comparator.comparing(x->x.price)).collect(Collectors.toList());
+        }
+        return book;
+    }
+}
