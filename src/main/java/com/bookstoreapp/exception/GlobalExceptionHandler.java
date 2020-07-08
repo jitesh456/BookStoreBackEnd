bf27a2f95f677b09b257001708def800f542dcfc
kn52 <ashish52922@gmail.com>
2020-07-09 00:03:36 +0530

    REFACTORED:Using global exception handler for all exception classes

--- src/main/java/com/bookstoreapp/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bookstoreapp/exception/GlobalExceptionHandler.java
@@ -1,4 +1,28 @@
 package com.bookstoreapp.exception;
 
+import com.bookstoreapp.response.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BookException.class)
+    public ResponseEntity<Response> bookHandler(BookException bookException){
+        return new ResponseEntity(new Response(bookException.message,
+                101,bookException.exceptionType),
+                HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(JwtTokenException.class)
+    public ResponseEntity<Response> bookHandler(JwtTokenException jwtTokenException){
+        return new ResponseEntity<Response>(new Response(jwtTokenException.message,
+                101, jwtTokenException.exceptionType), HttpStatus.OK);
+    }
+
+    @ExceptionHandler(UserException.class)
+    public ResponseEntity<Response> bookHandler(UserException userException) {
+        return new ResponseEntity<Response>(new Response(userException.message,
+                101, userException.exceptionType), HttpStatus.OK);
+    }
 }
